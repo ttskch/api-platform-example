ee097a30f1a2bc6b491781c1b9cb0d780a0311d2
Takashi Kanemoto <ttskch@gmail.com>
2022-05-05 13:47:11 +0900

    refactor: 💡 use ParamConverter

--- src/Controller/Comment/CollectionPostController.php
+++ src/Controller/Comment/CollectionPostController.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace App\Controller\Comment;
 
 use App\Entity\Comment;
-use App\Repository\PostRepository;
+use App\Entity\Post;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -13,14 +13,8 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 #[AsController]
 class CollectionPostController extends AbstractController
 {
-    public function __construct(private PostRepository $postRepository)
+    public function __invoke(Comment $data, ?Post $post): Comment
     {
-    }
-
-    public function __invoke(Comment $data, int $id): Comment
-    {
-        $post = $this->postRepository->find($id);
-
         if (!$post) {
             throw new NotFoundHttpException();
         }
