1a0b97e175387b24b603082b846fde43d64585ab
Takashi Kanemoto <ttskch@gmail.com>
2022-05-06 00:38:01 +0900

    feat: 🎸 disable Comment Item- Get, Put, Patch, Delete operations

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Action\NotFoundAction;
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Controller\Comment\CollectionPostController;
 use App\Controller\Comment\ItemBanController;
@@ -21,10 +22,11 @@ use Symfony\Component\Validator\Constraints as Assert;
         ],
     ],
     itemOperations: [
-        'get',
-        'put',
-        'patch',
-        'delete',
+        'get' => [
+            'controller' => NotFoundAction::class,
+            'read' => false,
+            'output' => false,
+        ],
         'ban' => [
             'method' => 'put',
             'path' => '/comments/{id}/ban',
