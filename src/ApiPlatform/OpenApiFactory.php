54250e3e26618bb7b2ce3617943098c5018f2676
Takashi Kanemoto <ttskch@gmail.com>
2022-05-06 14:13:33 +0900

    feat: 🎸 remove links for Comment Item Ban operation from API Doc

--- src/ApiPlatform/OpenApiFactory.php
+++ src/ApiPlatform/OpenApiFactory.php
@@ -8,6 +8,7 @@ use ApiPlatform\Core\OpenApi\Factory\OpenApiFactoryInterface;
 use ApiPlatform\Core\OpenApi\Model\Operation;
 use ApiPlatform\Core\OpenApi\Model\Parameter;
 use ApiPlatform\Core\OpenApi\Model\PathItem;
+use ApiPlatform\Core\OpenApi\Model\Response;
 use ApiPlatform\Core\OpenApi\OpenApi;
 
 class OpenApiFactory implements OpenApiFactoryInterface
@@ -44,6 +45,27 @@ class OpenApiFactory implements OpenApiFactoryInterface
                     /** @var Parameter[] $parameters */
                     $description = trim(strval(preg_replace('/#withoutRequestBody/', '', $operation->getDescription())));
                     $openApi->getPaths()->addPath($path, $pathItem = $pathItem->$setter($operation->withDescription($description)));
+                }
+            }
+
+            // remove links in operations which include "#withoutLinks" in description from API Doc
+            foreach (PathItem::$methods as $method) {
+                $getter = 'get'.ucfirst(strtolower($method));
+                $setter = 'with'.ucfirst(strtolower($method));
+                /** @var Operation|null $operation */
+                $operation = $pathItem->$getter();
+                if ($operation && preg_match('/#withoutLinks/', $operation->getDescription())) {
+                    $responses = [];
+                    /** @var Response $response */
+                    foreach ($operation->getResponses() as $statusCode => $response) {
+                        // use reflection because $operation->requestBody cannot be reset to null except in the constructor
+                        $reflectionProperty = new \ReflectionProperty($response, 'links');
+                        $reflectionProperty->setAccessible(true);
+                        $reflectionProperty->setValue($response, null);
+                        $responses[$statusCode] = $response;
+                    }
+                    $description = trim(strval(preg_replace('/#withoutLinks/', '', $operation->getDescription())));
+                    $openApi->getPaths()->addPath($path, $pathItem = $pathItem->$setter($operation->withDescription($description)->withResponses($responses)));
                 }
             }
         }
