c35373dbc590ab6159eccc85fcb09cab8a9801ad
Takashi Kanemoto <ttskch@gmail.com>
2022-05-06 17:56:10 +0900

    feat: 🎸 enable to define Collection Get operation with URI parameter

--- src/DataProvider/Comment/CollectionDataProvider.php
+++ src/DataProvider/Comment/CollectionDataProvider.php
@@ -9,6 +9,7 @@ use ApiPlatform\Core\DataProvider\RestrictedDataProviderInterface;
 use App\DataProvider\AbstractCollectionDataProvider;
 use App\Entity\Comment;
 use App\Repository\CommentRepository;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /**
  * @extends AbstractCollectionDataProvider<Comment>
@@ -29,8 +30,17 @@ final class CollectionDataProvider extends AbstractCollectionDataProvider implem
      */
     public function getCollection(string $resourceClass, string $operationName = null, array $context = []): iterable
     {
+        if (!preg_match('#^/api/v1/posts/(\d+)/comments#', strval($context['request_uri']), $m)) {
+            throw new NotFoundHttpException();
+        }
+
+        $postId = intval($m[1]);
+
         $qb = $this->commentRepository->createQueryBuilder('co')
+            ->leftJoin('co.post', 'po')
+            ->andWhere('po.id = :postId')
             ->andWhere('co.isBanned = false')
+            ->setParameter('postId', $postId)
         ;
 
         return $this->getResult($qb, $resourceClass, $operationName, $context);
