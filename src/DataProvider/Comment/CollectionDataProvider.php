f054c3d250d6e81f56a1127028461ddc672e3c2a
Takashi Kanemoto <ttskch@gmail.com>
2022-05-06 12:07:33 +0900

    feat: 🎸 apply collection-extensions to collection of Comments

--- src/DataProvider/Comment/CollectionDataProvider.php
+++ src/DataProvider/Comment/CollectionDataProvider.php
@@ -4,14 +4,17 @@ declare(strict_types=1);
 
 namespace App\DataProvider\Comment;
 
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\ContextAwareQueryResultCollectionExtensionInterface;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGenerator;
 use ApiPlatform\Core\DataProvider\ContextAwareCollectionDataProviderInterface;
 use ApiPlatform\Core\DataProvider\RestrictedDataProviderInterface;
 use App\Entity\Comment;
 use App\Repository\CommentRepository;
+use Doctrine\ORM\QueryBuilder;
 
 final class CollectionDataProvider implements ContextAwareCollectionDataProviderInterface, RestrictedDataProviderInterface
 {
-    public function __construct(private CommentRepository $commentRepository)
+    public function __construct(private iterable $collectionExtensions, private CommentRepository $commentRepository)
     {
     }
 
@@ -25,9 +28,31 @@ final class CollectionDataProvider implements ContextAwareCollectionDataProvider
      */
     public function getCollection(string $resourceClass, string $operationName = null, array $context = []): iterable
     {
-        /** @var iterable<Comment> $comments */
-        $comments = $this->commentRepository->findBy(['isBanned' => false]);
+        $qb = $this->commentRepository->createQueryBuilder('co')
+            ->andWhere('co.isBanned = false')
+        ;
 
-        return $comments;
+        /** @var iterable<Comment> $comments */
+        $result = $this->getResult($qb, $resourceClass, $operationName, $context);
+
+        return $result;
+    }
+
+    private function getResult(QueryBuilder $qb, string $resourceClass, ?string $operationName, array $context): iterable
+    {
+        $resultExtension = null;
+
+        foreach ($this->collectionExtensions as $extension) {
+            $extension->applyToCollection($qb, $generator ??= new QueryNameGenerator(), $resourceClass, $operationName, $context);
+
+            if ($extension instanceof ContextAwareQueryResultCollectionExtensionInterface) {
+                $resultExtension = $extension;
+            }
+        }
+
+        /** @var iterable $result */
+        $result = $resultExtension ? $resultExtension->getResult($qb, $resourceClass, $operationName, $context) : $qb->getQuery()->getResult();
+
+        return $result;
     }
 }
